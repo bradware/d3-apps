c9559f19a8b5bdc5921074723d3b14203e31c0d1
Bradford Ware <bware8@gatech.edu>
2016-03-28 23:46:17 -0400

    almost done

--- hw9/main.js
+++ hw9/main.js
@@ -2,14 +2,12 @@
 // has finished loading. In this case, onload() points to the
 // start() method we defined below. Because of something called
 // function hoisting, the start() method is callable below
-window.onload = start;
+window.onload = drawGraphs;
 
-function start() {
-  drawScatterPlot();
-  drawBarGraph();
-}
+var globalScatData = new Array();
+var globalBarData = new Array();
 
-function drawScatterPlot() {
+function drawGraphs() {
   var scatMargin = {top: 50, right: 0, bottom: 30, left: 0};
   var scatWidth = 600 - scatMargin.left - scatMargin.right;
   var scatHeight = 600 - scatMargin.top - scatMargin.bottom;
@@ -60,6 +58,7 @@ function drawScatterPlot() {
     });
 
     console.log(scatData);
+    globalScatData = scatData;
 
     // don't want dots overlapping axis, so add in buffer to scatData domain
     xScatScale.domain([d3.min(scatData, xScatValue)-1, d3.max(scatData, xScatValue)+1]);
@@ -119,6 +118,25 @@ function drawScatterPlot() {
               tooltip.style('opacity', 0);
               // TODO: resize the nodes
 
+          })
+          .on('click', function(d) {
+              var currCals = d.calories;
+              var matchingData = new Array();
+              console.log(currCals);
+              globalBarData.forEach(function(d) {
+                if (d.avgCals > currCals) {
+                  matchingData.push(d);
+                }
+              });
+              console.log(matchingData);
+              barSvg.selectAll('.bar')
+                  .data(matchingData)
+                  .transition()
+                    .delay(200)
+                    .duration(1000)
+                    .attr('fill', 'green');
+
+                
           });
 
     // draw legend
@@ -145,117 +163,133 @@ function drawScatterPlot() {
         .style('text-anchor', 'end')
         .text(function(d) { return d;});
   });
-}
 
-function drawBarGraph() {
-    // constant variables defined used throughout
-    var barGraph = document.getElementById('bar-graph');
+  // constant variables defined used throughout
+  var barGraph = document.getElementById('bar-graph');
 
-    var barWidth = 700;
-    var barHeight = 700;
+  var barWidth = 700;
+  var barHeight = 700;
 
-    var barSvg = d3.select(barGraph)
-        .append('svg')
-        .attr('width', barWidth)
-        .attr('height', barHeight);
-    var bars = barSvg.append('g');
+  var barSvg = d3.select(barGraph)
+      .append('svg')
+      .attr('width', barWidth)
+      .attr('height', barHeight);
+  var bars = barSvg.append('g');
 
-    // scales and y-axis for graph
-    var xBarScale = d3.scale.linear().range([0, barWidth]);
-    var yBarScale = d3.scale.ordinal().rangeRoundBands([0, barHeight], 0.3);
-    var yBarAxis = d3.svg.axis().scale(yBarScale).orient('left');
+  // scales and y-axis for graph
+  var xBarScale = d3.scale.linear().range([0, barWidth]);
+  var yBarScale = d3.scale.ordinal().rangeRoundBands([0, barHeight], 0.3);
+  var yBarAxis = d3.svg.axis().scale(yBarScale).orient('left');
 
-    var refreshBars = function() {
-            bars.selectAll('.bar')
-                .transition()
-                    .delay(200)
-                    .duration(1000)
-                    .style('fill', '#0066CC')
-                    .attr('width', function(d) {
-                        return xBarScale(d.avgCals);
-                    });
-        };
+  var refreshBars = function() {
+          bars.selectAll('.bar')
+              .transition()
+                  .delay(200)
+                  .duration(1000)
+                  .style('fill', '#0066CC')
+                  .attr('width', function(d) {
+                      return xBarScale(d.avgCals);
+                  });
+      };
 
-    var filterBars = function(filterVal, filterFunc) {
-        bars.selectAll('.bar')
-            .filter(filterFunc)
-            .transition()
-                .delay(200)
-                .duration(1000)
-                .style('fill', 'red')
-                .attr('width', function(d) {
-                    return xBarScale(0);
-                });
-    }; 
+  var filterBars = function(filterVal, filterFunc) {
+      bars.selectAll('.bar')
+          .filter(filterFunc)
+          .transition()
+              .delay(200)
+              .duration(1000)
+              .style('fill', 'red')
+              .attr('width', function(d) {
+                  return xBarScale(0);
+              });
+  }; 
 
-    var map = {};
-    d3.csv('data/cereal.csv', function(d) {
-        if (d.Manufacturer in map) {
-            var newCals = map[d.Manufacturer].totalCals + (+d.Calories);
-            var newCount = map[d.Manufacturer].count + 1;
-            var newAvg = Math.round(newCals / newCount);
-            map[d.Manufacturer] = {manu: d.Manufacturer, totalCals: newCals, count: newCount, avgCals: newAvg};
-        } 
-        else {
-            map[d.Manufacturer] = {manu: d.Manufacturer, totalCals: +d.Calories, count: 1, avgCals: +d.Calories};
-        }
-    }, 
-    function(error, barData) {
-        if (error) throw error;
-        barData = new Array();
-        for (var prop in map) { barData.push(map[prop]); }
-        // log barData to console for inspection
-        console.log(barData);
-        // We set the domain of the xScale. The domain includes 0 up to
-        // the maximum frequency in the barDataset. This is because 
-        xBarScale.domain([0, d3.max(barData, function(d) {
-            return d.avgCals;
-        })]);
+  var map = {};
+  d3.csv('data/cereal.csv', function(d) {
+      if (d.Manufacturer in map) {
+          var newCals = map[d.Manufacturer].totalCals + (+d.Calories);
+          var newCount = map[d.Manufacturer].count + 1;
+          var newAvg = Math.round(newCals / newCount);
+          map[d.Manufacturer] = {manu: d.Manufacturer, totalCals: newCals, count: newCount, avgCals: newAvg};
+      } 
+      else {
+          map[d.Manufacturer] = {manu: d.Manufacturer, totalCals: +d.Calories, count: 1, avgCals: +d.Calories};
+      }
+  }, 
+  function(error, barData) {
+      if (error) throw error;
+      for (var prop in map) { barData.push(map[prop]); }
+      // log barData to console for inspection
+      console.log(barData);
+      globalBarData = barData;
+      // We set the domain of the xScale. The domain includes 0 up to
+      // the maximum frequency in the barDataset. This is because 
+      xBarScale.domain([0, d3.max(barData, function(d) {
+          return d.avgCals;
+      })]);
 
-        // We set the domain of the yScale. The scale is ordinal, and
-        // contains every letter in the alphabet (the letter attribute
-        // in our barData array). We can use the map function to iterate
-        // through each value in our barData array, and make a new array
-        // that contains just letters.
-        yBarScale.domain(barData.map(function(d) {
-            return d.manu;
-        }));
+      // We set the domain of the yScale. The scale is ordinal, and
+      // contains every letter in the alphabet (the letter attribute
+      // in our barData array). We can use the map function to iterate
+      // through each value in our barData array, and make a new array
+      // that contains just letters.
+      yBarScale.domain(barData.map(function(d) {
+          return d.manu;
+      }));
 
-        // Append the y-axis to the graph. the translate(20, 0) stuff
-        // shifts the axis 20 pixels from the left. This just helps us
-        // position stuff to where we want it to be.
-        bars.append('g')
-            .attr('class', 'y axis')
-            .attr('transform', 'translate(70, 0)')
-            // Call is a special method that lets us invoke a function
-            // (called 'yAxis' in this case) which creates the actual
-            // yAxis using D3.
-            .call(yBarAxis);
+      // Append the y-axis to the graph. the translate(20, 0) stuff
+      // shifts the axis 20 pixels from the left. This just helps us
+      // position stuff to where we want it to be.
+      bars.append('g')
+          .attr('class', 'y axis')
+          .attr('transform', 'translate(70, 0)')
+          // Call is a special method that lets us invoke a function
+          // (called 'yAxis' in this case) which creates the actual
+          // yAxis using D3.
+          .call(yBarAxis);
 
-        // Create the bars in the graph. First, select all '.bars' that
-        // currently exist, then load the barData into them. enter() selects
-        // all the pieces of barData and lets us operate on them.
-        bars.append('g')
-            .selectAll('.bar')
-            .data(barData)
-            .enter()
-            .append('rect')
-                .attr('class', 'bar')
-                .attr('x', 80)
-                .attr('y', function(d) {
-                    return yBarScale(d.manu);
-                })
-                .attr('width', function(d) {
-                    // xScale will map any number and return a number
-                    // within the output range we specified earlier.
-                    return xBarScale(d.avgCals);
-                })
-                .attr('height', function(d) {
-                    // Remember how we set the yScale to be an ordinal scale
-                    // with bands from 0 to height? And then we set the domain 
-                    // to contain all the letters in the alphabet? 
-                    return yBarScale.rangeBand();
-                });
+      // Create the bars in the graph. First, select all '.bars' that
+      // currently exist, then load the barData into them. enter() selects
+      // all the pieces of barData and lets us operate on them.
+      bars.append('g')
+          .selectAll('.bar')
+          .data(barData)
+          .enter()
+          .append('rect')
+              .attr('class', 'bar')
+              .attr('x', 80)
+              .attr('y', function(d) {
+                  return yBarScale(d.manu);
+              })
+              .attr('width', function(d) {
+                  // xScale will map any number and return a number
+                  // within the output range we specified earlier.
+                  return xBarScale(d.avgCals);
+              })
+              .attr('height', function(d) {
+                  // Remember how we set the yScale to be an ordinal scale
+                  // with bands from 0 to height? And then we set the domain 
+                  // to contain all the letters in the alphabet? 
+                  return yBarScale.rangeBand();
+              })
+              .on('click', function(d) {
+                  currManu = d.manu;
+                  var matchingData = new Array();
+                  console.log(currManu);
+                  globalScatData.forEach(function(d) {
+                    if (d.Manufacturer !== currManu) {
+                      console.log('in here');
+                      matchingData.push(d);
+                    }
+                  });
+                  console.log(matchingData);
+                  scatSvg.selectAll('.dot')
+                          .data(matchingData)
+                          .transition()
+                            .delay(200)
+                            .duration(1000)
+                            .style('opacity', 0.25);
+              });
 
-    });
+  });
 }
